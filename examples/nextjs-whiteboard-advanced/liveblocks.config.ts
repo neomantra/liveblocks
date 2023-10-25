429a1dbcdce99bd7aa22788919f2e2e9163c9e9f
Vincent Driessen <me@nvie.com>
2023-10-25 17:09:28 +0200

    Fake auth, and add debug logging

--- examples/nextjs-whiteboard-advanced/liveblocks.config.ts
+++ examples/nextjs-whiteboard-advanced/liveblocks.config.ts
@@ -5,9 +5,18 @@ import { Point, Color, Layer } from "./src/types";
 
 const client = createClient({
   throttle: 16,
-  authEndpoint: "/api/liveblocks-auth",
+
+  // authEndpoint: "/api/liveblocks-auth",
+  authEndpoint: (_roomId: string) =>
+    Promise.resolve({
+      token:
+        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTgyNDI2ODksImV4cCI6MTY5ODI0NjI4OSwiayI6ImFjYyIsInBpZCI6IjYzNTc4ZWFlYzM4ODRlYjYxZWZkNjc1NSIsInVpZCI6InVzZXItNiIsInBlcm1zIjp7Im5leHRqcy13aGl0ZWJvYXJkLWFkdmFuY2VkIjpbInJvb206d3JpdGUiLCJjb21tZW50czp3cml0ZSJdfSwibWNwciI6MjB9.lt97pzb7xB3snpSeqixdLiqt3Lk2G3pFCsCMdQkLDW38x8WX_HaQYAGgNtAS_G-9MvJJirr-d6CnqvXfdei1Ngc6AoQWVFPu-ml5LqgnL160VYH78ZhDwGS1do6ICPR1bOeFzwblvIlfvPd-7k_9oFkPNGyi_0DVd41pq3pYozW_vT78Zt-LGVATP15umx1Ue5zJ19VbFVLFI244Z6bI8TGo6cTK4YwSlgkPy22bj5faZ0eSL04m57HFP_9-_0MyEbwXCuHDrtBHpOlxxZHw8bNfV6bgyzJ1KCZj9GKN-hzKb5ekL9IoFRwhPK3RH3gJChh7BuBD3zlyskTTDcIApQ",
+    }),
+
   // @ts-expect-error Hidden config
   baseUrl: "http://localhost:3333/",
+
+  enableDebugLogging: true,
 });
 
 // Presence represents the properties that will exist on every User in the Room
